f7056a582625e1fa5ffb9abd41e97fdd64a5f21d
Mozib <mozib.diu@gmail.com>
2022-10-19 14:16:43 +0600

    working

--- api/api.php
+++ api/api.php
@@ -6,7 +6,9 @@ $html = file_get_html($gameUrl);
 // echo $html;
 // ftb_details_header_status
 // ftb_details_header_foot
+// overview_main_title
 
+$league = $html->find('h2.overview_main_title a', 0)->plaintext;
 $teamOne = $html->find('div.ftb_details_header_name_pic h3', 0)->plaintext;
 $teamTwo = $html->find('div.ftb_details_header_name_pic h3', 1)->plaintext;
 $time = $html->find('div.ftb_details_header_status label', 0)->plaintext;
@@ -14,7 +16,7 @@ $score = $html->find('div.ftb_details_header_status h3 a', 0)->plaintext;
 $dateOfPlay = $html->find('div.ftb_details_header_foot ul li span samp.mdate', 0)->plaintext;
 $timeOfPlay = $html->find('div.ftb_details_header_foot ul li span samp.mtime', 0)->plaintext;
 
-$data = ['teamOne'=>$teamOne,'teamTwo'=>$teamTwo,'time'=>$time,'score'=>$score,'dateOfPlay'=>$dateOfPlay,'timeOfPlay'=>$timeOfPlay];
+$data = ['league'=>$league,'teamOne'=>$teamOne,'teamTwo'=>$teamTwo,'time'=>$time,'score'=>$score,'dateOfPlay'=>$dateOfPlay,'timeOfPlay'=>$timeOfPlay];
 echo json_encode($data);
 
 file_put_contents('../api/data.json', json_encode($data));
