9f6e36a3269df4f96d6573175c173babd628f2da
Mozib <mozib.diu@gmail.com>
2022-10-19 11:22:25 +0600

    File done

--- api/api.php
+++ api/api.php
@@ -1,18 +1,20 @@
 <?php
 include_once('./simple_html_dom.php');
-$html = file_get_html("https://livescore.football365.com/spain-laliga-sevilla-vs-valencia/18-10-2022/2144356/overview");
+$html = file_get_html("https://livescore.football365.com/england-fa-cup-qualification-chester-fc-vs-oldham-athletic/19-10-2022/2276051/overview");
 
 // echo $html;
 // ftb_details_header_status
-
+// ftb_details_header_foot
 
 $teamOne = $html->find('div.ftb_details_header_name_pic h3', 0)->plaintext;
 $teamTwo = $html->find('div.ftb_details_header_name_pic h3', 1)->plaintext;
 $time = $html->find('div.ftb_details_header_status label', 0)->plaintext;
 $score = $html->find('div.ftb_details_header_status h3 a', 0)->plaintext;
+$dateOfPlay = $html->find('div.ftb_details_header_foot ul li span samp.mdate', 0)->plaintext;
+$timeOfPlay = $html->find('div.ftb_details_header_foot ul li span samp.mtime', 0)->plaintext;
 
-$data = ['teamOne'=>$teamOne,'teamTwo'=>$teamTwo,'time'=>$time,'score'=>$score];
+$data = ['teamOne'=>$teamOne,'teamTwo'=>$teamTwo,'time'=>$time,'score'=>$score,'dateOfPlay'=>$dateOfPlay,'timeOfPlay'=>$timeOfPlay];
 echo json_encode($data);
 
-file_put_contents('./footballApi/footballData.json', json_encode($data));
+file_put_contents('../api/data.json', json_encode($data));
 ?>
---
+++ front/script.js
@@ -0,0 +1,14 @@
+fetch('http://localhost/ad-rabbithole/api/data.json')
+.then(res => res.json())
+.then(data => dataOperation(data))
+
+function dataOperation(data) {
+  score = data.score.split(' ');
+  teamOneScore =score[0];
+  teamTwoScore = score[2];
+    // teamOneScore = data.score[0];
+    // teamTwoScore = data.score.slice(-1);
+
+    console.log(teamOneScore,teamTwoScore);
+    console.log(score);
+}
