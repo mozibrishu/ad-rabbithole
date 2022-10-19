9d8c7901118b978ea6d12fb005dfbf1434976355
Mozib <mozib.diu@gmail.com>
2022-10-19 10:27:18 +0600

    Basic files ready

---
+++ api/api.php
@@ -0,0 +1,18 @@
+<?php
+include_once('./simple_html_dom.php');
+$html = file_get_html("https://livescore.football365.com/spain-laliga-sevilla-vs-valencia/18-10-2022/2144356/overview");
+
+// echo $html;
+// ftb_details_header_status
+
+
+$teamOne = $html->find('div.ftb_details_header_name_pic h3', 0)->plaintext;
+$teamTwo = $html->find('div.ftb_details_header_name_pic h3', 1)->plaintext;
+$time = $html->find('div.ftb_details_header_status label', 0)->plaintext;
+$score = $html->find('div.ftb_details_header_status h3 a', 0)->plaintext;
+
+$data = ['teamOne'=>$teamOne,'teamTwo'=>$teamTwo,'time'=>$time,'score'=>$score];
+echo json_encode($data);
+
+file_put_contents('./footballApi/footballData.json', json_encode($data));
+?>
